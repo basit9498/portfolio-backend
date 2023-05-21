c0fa9e279e75a230db376641665ccfe7dc618f27
Abdul Basit <basit9498@gnail.com>
2023-05-21 18:57:32 +0500

    Config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -14,7 +14,7 @@ import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL!);
+    await mongoose.connect(process.env.DATABASE_URL as string);
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection error:', error);
